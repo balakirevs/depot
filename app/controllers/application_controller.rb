8b14a73e21d329d93bf24a40ebda1b3b0c4a06c3
Alexander Balakirev <balakirevs@i.ua>
2012-03-22 00:26:05 +0200

    Loggin in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
   protect_from_forgery
 
-   private
+  private
 
     def current_cart 
       Cart.find(session[:cart_id])
@@ -10,4 +11,15 @@ class ApplicationController < ActionController::Base
       session[:cart_id] = cart.id
       cart
     end
+
+    # ...
+
+  protected
+
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, :notice => "Please log in"
+      end
+    end
 end
+
