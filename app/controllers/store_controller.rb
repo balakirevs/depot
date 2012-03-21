1498f392ea30c36ba22e02327be47581be226bc2
Alexander Balakirev <balakirevs@i.ua>
2012-03-21 17:24:31 +0200

    AJAX

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,10 @@
+class StoreController < ApplicationController
+  def index
+  	@products = Product.all
+  	@counter = session[:counter]
+    @counter.nil? ? @counter = 1 : @counter+=1
+    session[:counter] = @counter
+  	@current_time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+  	@cart = current_cart
+  end
+end
