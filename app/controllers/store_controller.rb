eb51e27cab181c7faa1cb095a1994bd48542543d
Alexander Balakirev <balakirevs@i.ua>
2012-03-22 02:24:26 +0200

    Internationalization

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,15 @@
 class StoreController < ApplicationController
   skip_before_filter :authorize	
   def index
-  	@products = Product.all
-  	@counter = session[:counter]
-    @counter.nil? ? @counter = 1 : @counter+=1
-    session[:counter] = @counter
-  	@current_time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-  	@cart = current_cart
+  	if params[:set_locale]
+       redirect_to store_path(:locale => params[:set_locale])
+    else
+  	  @products = Product.all
+  	  @counter = session[:counter]
+      @counter.nil? ? @counter = 1 : @counter+=1
+      session[:counter] = @counter
+  	  @current_time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+  	  @cart = current_cart
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,18 @@ Depot::Application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
+  scope '(:locale)' do
   resources :users
-
   resources :pay_types
-
   resources :orders
-
   resources :line_items
-
   resources :carts
-
-  get "store/index"
-
   resources :products do
     get :who_bought, :on => :member
-  end  
+  end
+    root :to => 'store#index', :as => 'store'
+  end
+end    
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -73,10 +69,8 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-    root :to => 'store#index' , :as => 'store'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
